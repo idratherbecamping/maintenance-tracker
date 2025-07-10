171e7c95ea64341cf41bef466fb68c14432573fe
Gannon Gesiriech <ggesiriech@gmail.com>
2025-07-10 13:21:12 -0700

    remove a link to signup

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -54,12 +54,7 @@ export default function LoginPage() {
           <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
             Sign in to Maintenance Tracker
           </h2>
-          <p className="mt-2 text-center text-sm text-gray-600">
-            Or{' '}
-            <Link href="/signup" className="font-medium text-blue-600 hover:text-blue-500">
-              create a new account
-            </Link>
-          </p>
+
         </div>
 
         <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
